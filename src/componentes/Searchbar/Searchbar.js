c184a856fae1d5408ff2dce3b431f59836eac19d
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2022-07-26 22:30:03 -0300

    CSS searchbar

--- src/componentes/Searchbar/Searchbar.js
+++ src/componentes/Searchbar/Searchbar.js
@@ -1,10 +1,12 @@
 import React from "react";
+import '../Searchbar/Searchbar.css';
 import { searchPokemon } from "../../Api";
 const {useState} = React;
 
 const Searchbar = () => {
 
     const [search, setSearch] = useState("");
+    const [/*pokemon*/ setPokemon] = useState();
 
     const onChange = (e) => {
         setSearch(e.target.value);
@@ -12,15 +14,15 @@ const Searchbar = () => {
 
     const onClick = async (e) => {
         const data = await searchPokemon(search);
-        console.log(data);
+        setPokemon(data);
     };
 
     return (
-        <div>
-            <div>
+        <div className="searchbar-container">
+            <div className="searchbar">
                 <input placeholder="Buscar pokemon" onChange={onChange} />
             </div>
-            <div>
+            <div className="searchbar-btn">
                 <button onClick={onClick}>Buscar</button>
             </div>
         </div>
