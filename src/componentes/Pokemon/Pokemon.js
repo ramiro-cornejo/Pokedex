0a435a946675c235febedc35f332dff7088bf268
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2022-08-02 12:35:23 -0300

    Style en pokedex

--- src/componentes/Pokemon/Pokemon.js
+++ src/componentes/Pokemon/Pokemon.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../Pokemon/Pokemon.css'
 
 const Pokemon = (props) => {
     const {pokemon} = props;
@@ -6,24 +7,25 @@ const Pokemon = (props) => {
 
     return(
         <div className="pokemon-card">
-            <div className="pokemon-img">
+            <div className="pokemon-img-container">
                 <img src={pokemon.sprites.front_default}
                 alt={pokemon.name}
+                className="pokemon-img"
                 />
             </div>
-            <div>
-                <div>
+            <div className="card-body">
+                <div className="card-top">
                     <h3>{pokemon.name}</h3>
                     <div>#{pokemon.id}</div>
                 </div>
-                <div>
-                    <div>{pokemon.types.map((type, idx) => {
+                <div className="card-botton">
+                    <div className="pokemon-type">{pokemon.types.map((type, idx) => {
                         return (
-                            <div key={idx}>{type.name}</div>
+                            <div key={idx} className="pokemon-type-text" >{type.type.name}</div>
                         )
                     })}
                     </div>
-                    <div>❤</div>
+                    <div className="pokemon-favorite">❤</div>
                 </div>
             </div>
         </div>
